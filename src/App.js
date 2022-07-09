0355d18bf67d8b6543fed2739f4488f6c45074e5
tangzhenming <15977536060@163.com>
2022-07-10 03:15:02 +0800

    deploy done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test CD in github workflow 2</p>
+        <p>Test CD in github workflow</p>
         <a
           className="App-link"
           href="https://reactjs.org"
